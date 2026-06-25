6aab236cbdc986de3e936f297b34fc0edd02194c
Paradis Keza <p.keza1@alustudent.com>
2026-06-25 17:07:03 +0200

    Complete task 9 print last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 def print_last_digit(number):
-    last = abs(number) % 10
-    print(last, end="")
-    return last
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
