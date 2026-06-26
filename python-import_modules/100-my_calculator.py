527caa6b13d5f9752e0a98b04d88f46adcaf1d3e
Paradis Keza <p.keza1@alustudent.com>
2026-06-26 10:44:15 +0200

    Advanced tasks

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    result = operations[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
