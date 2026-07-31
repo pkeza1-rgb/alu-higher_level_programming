540d0fd525d647d02a4a2c448117f724e8960cf7
Paradis Keza <p.keza1@alustudent.com>
2026-07-31 13:03:26 +0200

    Fixed errors

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
--- Creates the MySQL user user_0d_1
+-- Creates MySQL user user_0d_1
 
 -- Create user if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
--- Give all privileges to user
+-- Grant all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
